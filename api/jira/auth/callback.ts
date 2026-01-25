f25f086d6145e746b7546e0936bd9789bb556a66
lavansh1306 <lavansh1201a@gmail.com>
2026-01-25 22:57:35 +0530

    trying to make servesless work

--- api/jira/auth/callback.ts
+++ api/jira/auth/callback.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { handleJiraCallback } from '../auth';
+import { handleJiraCallback } from '../auth.js';
 
 export default async function handler(req: Request, res: Response) {
   try {
--- api/jira/auth/connect.ts
+++ api/jira/auth/connect.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { handleJiraConnect } from '../auth';
+import { handleJiraConnect } from '../auth.js';
 
 export default async function handler(req: Request, res: Response) {
   try {
