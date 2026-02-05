8e40b7df60f3b4817975637eb3513c4ea29da06b
lavansh1306 <lavansh1201a@gmail.com>
2026-02-05 22:45:52 +0530

    remove vercel analystics

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import { ToastProvider } from "@/contexts/ToastContext";
 import { ToastContainer } from "@/components/ToastContainer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import Index from "./pages/Index";
 import Demo from "./pages/Demo";
 import ROICalculator from "./pages/ROICalculator";
@@ -30,8 +28,6 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ToastProvider>
       <TooltipProvider>
-        <Analytics />
-        <SpeedInsights />
         <Toaster />
         <Sonner />
         <ToastContainer />
