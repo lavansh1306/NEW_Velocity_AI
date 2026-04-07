333f679a0592b0e224917cfc33b9f41161fa8030
Claire Kwon <0304gkdud@gmail.com>
2026-04-06 17:10:15 -0700

    feat: sample data banner + service

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { DashboardScreen } from '@/components/DashboardScreen';
+import { SampleDataBanner } from '@/components/SampleDataBanner';
 import { VelocityAISidebar } from '@/components/dashboard/VelocityAISidebar';
 
 const DashboardPage = () => {
   return (
     <VelocityAISidebar>
       <div className="min-h-screen bg-[#FAFAF9]">
+        <SampleDataBanner />
         <DashboardScreen />
       </div>
     </VelocityAISidebar>
